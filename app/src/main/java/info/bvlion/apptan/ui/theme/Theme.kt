7e2e11e52fba752464e6b7c83e5cc9bcbbdd81eb
bvlion <bvltiggeari@ambitious-i.net>
2025-04-24 00:11:09 +0900

    first commit

---
+++ app/src/main/java/info/bvlion/apptan/ui/theme/Theme.kt
@@ -0,0 +1,20 @@
+package info.bvlion.apptan.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+@Composable
+fun AppTanTheme(
+  darkTheme: Boolean = isSystemInDarkTheme(),
+  content: @Composable () -> Unit
+) {
+  val context = LocalContext.current
+  MaterialTheme(
+    colorScheme = if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context),
+    content = content
+  )
+}
